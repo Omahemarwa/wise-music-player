60661c9cd79f04f00cd2c67f43c6a4e5dc561d0c
OMAHE DANIEL <168162111+Omahemarwa@users.noreply.github.com>
2026-09-15 17:00:04 +0300

    Auto detecting the song

--- android/app/src/main/java/com/wise/musicplayer/MainActivity.java
+++ android/app/src/main/java/com/wise/musicplayer/MainActivity.java
@@ -1,5 +1,13 @@
 package com.wise.musicplayer;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Must be registered BEFORE super.onCreate()
+        registerPlugin(MediaStoreAudioPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
